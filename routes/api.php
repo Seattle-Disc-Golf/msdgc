434cc3eed0c5df49165c79704c3128922b80765a
Rowan Christmas <rowanxmas@gmail.com>
2025-07-21 21:29:44 -0700

    remove deprecated

--- routes/api.php
+++ routes/api.php
@@ -28,11 +28,3 @@ Route::middleware(['auth:sanctum'])->group(function () {
 // CMS Collections API routes
 Route::get('/collections/{collection}', [CMSCollectionsController::class, 'index']);
 Route::get('/collections/{collection}/{id}', [CMSCollectionsController::class, 'show']);
-
-// Backward compatibility routes (optional - can be removed later)
-Route::get('/sponsors', function() {
-    return app(CMSCollectionsController::class)->index('sponsors');
-});
-Route::get('/sponsors/{id}', function(string $id) {
-    return app(CMSCollectionsController::class)->show('sponsors', $id);
-});
